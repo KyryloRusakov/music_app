75e42322fcfdd71027ef7137ce400bcffeed98da
Kyrylo Rusakov <kyrylo.rusakov@nixsolutions.com>
2024-05-16 19:03:26 +0300

    add files

---
+++ src/components/MusicPlayer/Track.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Track = ({ isPlaying, isActive, activeSong }) => (
+  <div className="flex-1 flex items-center justify-start">
+    <div className={`${isPlaying && isActive ? 'animate-[spin_3s_linear_infinite]' : ''} hidden sm:block h-16 w-16 mr-4`}>
+      <img src={activeSong?.album.images[0].url} alt="cover art" className="rounded-full" />
+    </div>
+    <div className="w-[50%]">
+      <p className="truncate text-white font-bold text-lg">
+        {activeSong?.name ? activeSong?.name : 'No active Song'}
+      </p>
+      {
+        activeSong?.artists ? 
+        activeSong?.artists.map((artistsName) => (
+          <p className="truncate text-gray-300">{artistsName.name}</p>
+        )) : 'No active Song'
+      }
+    </div>
+  </div>
+);
+
+export default Track;
---
+++ src/pages/SongDetails.jsx
@@ -0,0 +1,40 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { DetailsHeader, Error, Loader } from "../components";
+
+import { useGetSongDetailsQuery, useGetTopChartsQuery } from "../redux/services/shazamCore";
+
+const SongDetails = () => {
+  const dispatch = useDispatch();
+  const { songid } = useParams();
+  const { data: songData, isFetching: isFetchingSongDetails, error } = useGetSongDetailsQuery({ songid });
+  const { data: charts } = useGetTopChartsQuery();
+  console.log(songData);
+  console.log(charts);
+
+  if (isFetchingSongDetails) return <Loader title="Searching song details"/>;
+
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader artistId="" songData={charts} songId={songid}/>
+
+      <div className="mb-10">
+        <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
+
+        <div className="mt-5">
+          {songData?.lyrics ? (
+            songData.lyrics.lines.map((line, i) => (
+              <p key={i} className="text-gray-400 text-base my-1">{line.words}</p>
+            ))
+          ) : (
+            <p className="text-gray-400 text-base my-1">Lyrics not found</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SongDetails;
